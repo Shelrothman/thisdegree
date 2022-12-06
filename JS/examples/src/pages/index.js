032fff1e9861dea02d95e83a8d8f65e9b5de5427
Shelby Rothman <shelby.rothman@aderant.com>
2022-12-06 12:37:35 -0800

    cleanup cruft

--- JS/examples/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Dashboard from './Dashboard';
-import Samples from './Samples';
-import Profile from './Profile';
-import Logout from './Logout';
-import Sample from './Sample';
-
-export { Dashboard, Samples, Profile, Logout, Sample };
