39a474d946b8ed7d6a7964a159c7741b7f09ff09
Shelby Rothman <shelby.rothman@aderant.com>
2022-12-22 08:58:44 -0800

    set up apolloClient in webapp

--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import './styles/index.css'
+
+import {
+    ApolloProvider,
+    ApolloClient,
+    createHttpLink,
+    InMemoryCache
+} from '@apollo/client';
+
+//the httpLink that will connect our ApolloClient instance with the GraphQL API which is running on loc
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000'
+});
+
+// initialize ApolloClient
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+    <ApolloProvider client={client}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </ApolloProvider>
+);
