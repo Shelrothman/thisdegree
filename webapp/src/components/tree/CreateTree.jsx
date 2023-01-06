d40b883fe878f1a4729c709ec862bf590b792dd1
Shelby Rothman <shelby.rothman@aderant.com>
2023-01-05 19:12:22 -0800

    dynamically rendering left padding

--- webapp/src/components/tree/CreateTree.jsx
+++ webapp/src/components/tree/CreateTree.jsx
@@ -6,6 +6,72 @@ import DataTree from './DataTree';
 
 // ! user must be authenticated to post a tree
 
+const TEST_TREE = `[
+    {
+        "startingActor": "Jennifer Aniston",
+        "id": "319f76a8-8302-83b4-0915-a7771a8e5fb2"
+    },
+    {
+        "id": "8e325b58-6319-bf1e-14ea-3da93232460e",
+        "movieTitle": "along came polly",
+        "previousActor": {
+            "name": "Jennifer Aniston",
+            "characterName": "Polly Prince"
+        },
+        "actorGuessed": true,
+        "actorSelection": {
+            "id": "053dc009-0447-102f-9d9e-5b0ea911220b",
+            "name": "Alec Baldwin",
+            "characterName": "Stan Indursky"
+        }
+    },
+    {
+        "id": "c4aa0a3b-c847-6692-79b8-2dab8742c86e",
+        "movieTitle": "arctic dogs",
+        "previousActor": {
+            "name": "Alec Baldwin",
+            "characterName": "PB (voice)"
+        },
+        "actorGuessed": true,
+        "actorSelection": {
+            "id": "f677671a-c87a-b2fb-ce10-ef094d7f0d8e",
+            "name": "James Franco",
+            "characterName": "Lemmy (voice)"
+        }
+    },
+    {
+        "id": "d8e4971e-2b94-5b0f-9a88-531c7e2544ac",
+        "movieTitle": "sausage party",
+        "previousActor": {
+            "name": "James Franco",
+            "characterName": "Druggie (voice)"
+        },
+        "actorGuessed": true,
+        "actorSelection": {
+            "id": "062d2bf5-95ab-1d41-01dc-7025811da0a8",
+            "name": "Kristen Wiig",
+            "characterName": "Brenda (voice)"
+        }
+    },
+    {
+        "id": "44abd38b-b1ec-cddd-4632-79557e8a5bd3",
+        "movieTitle": "A deadly adoption",
+        "previousActor": {
+            "name": "Kristen Wiig",
+            "characterName": "Sarah Benson"
+        },
+        "actorGuessed": true,
+        "actorSelection": {
+            "id": "c23c8994-5b2e-67c0-dbd2-4faa98f97bea",
+            "name": "Will Ferrell",
+            "characterName": "Robert Benson"
+        }
+    },
+    {
+        "endingActor": "Will Ferrell",
+        "id": "d8970a28-69a8-9380-9c2f-5a5db4cc6b98"
+    }
+]`
 
 
 
@@ -13,18 +79,21 @@ const CreateTree = () => {
     const navigate = useNavigate();
 
     // use the useLocation hook to get the state from the previous page
-    const { state } = useLocation();
-    console.log('state: ', state);
+    //! const { state } = useLocation();
+    //! console.log('state: ', state);
 
-    const [treeObject, setTreeObject] = useState(JSON.parse(state.tree));
+    //const [treeObject, setTreeObject] = useState(JSON.parse(state.tree));
+    const [treeObject, setTreeObject] = useState(JSON.parse(TEST_TREE));
+    
+    
+    
     console.log('treeObject: ', treeObject)
     console.log(typeof treeObject);
 
-    // TODO: play with using a similar setup as this in my scoreboard inputs (state-wise)
     const [formState, setFormState] = useState({
-        treeDeclaration: state.tree,
-        // treeDeclaration: 'test tree declaration',
-        // !!! using hard value for now for testing. return and update thus
+        // treeDeclaration: state.tree,
+        treeDeclaration: 'test tree declaration',
+        //! return to uncomment
     });
     // pass the CREATE_TREE_MUTATION to the useMutation hook 
     // and pass in the data provided in the input fields as variables.
@@ -67,7 +136,7 @@ const CreateTree = () => {
                 </div>
                 <button type="submit">Submit</button>
                 <pre style={{ whiteSpace: 'pre-wrap' }} >
-                        {JSON.stringify(JSON.parse(state.tree), null, 2)}
+                        {/* {JSON.stringify(JSON.parse(state.tree), null, 2)} */}
                 </pre>
                 <DataTree treeData={treeObject} />
             </form>
--- webapp/src/components/tree/DataTree.jsx
+++ webapp/src/components/tree/DataTree.jsx
@@ -1,46 +1,73 @@
+import { useEffect } from "react";
 import { useState } from "react";
+import { AiOutlineArrowDown } from "react-icons/ai";
 
-const TreeNode = ({ movieTitle, previousActor, actorSelection, level }) => (
-    <li style={{ paddingLeft: `${level * 20}px` }}>
-        {movieTitle} - {previousActor.name} ({previousActor.characterName}) --&gt;{" "}
-        {actorSelection.name} ({actorSelection.characterName})
+
+function TreeNode({ id, movieTitle, previousActor, actorSelection, level, children }) {
+    // setLevel(prevLevel => prevLevel + 1);
+    // const nextLevel = level + 1;
+    // setLevel(nextLevel);
+    // const [level, setLevel] = useState(0);
+
+// make level increase on each <li> node
+
+    
+
+// todo: when hover over the movie node, highlight its actors nodes with it in same color
+// good for me to know how this gets fone for future toruble shooting
+// highlight one actor node, the other actor node with same name and different character name gets highlighted with it with like a shadow or something.
+
+    return (
+        <li style={{ paddingLeft: `${level * 10}px` }}>
+            
+            {movieTitle.toUpperCase()} 
+            <br />
+            <AiOutlineArrowDown size={25} />
+            <br />
+            &emsp;{previousActor.name} ({previousActor.characterName}) 
+            <br />
+            &emsp;<AiOutlineArrowDown size={25} />
+            <br />
+            &emsp;{actorSelection.name} ({actorSelection.characterName})
+            <br />
+            &emsp;<AiOutlineArrowDown size={25} />
+            {children}
+        </li>
+    );
+}
+
+
+function DataTree({ treeData }) {
+    // useState for the level to increase dynamically
+    const [level, setLevel] = useState(0);
+
+    return (
         <ul>
-            {/* The children nodes of this tree node will be added here */}
+            {treeData
+                // filter out any nodes that don't have a movieTitle property (FOR NOW__ MAYBE WE CHANGE)
+                .filter(({ movieTitle }) => movieTitle)
+                .map(
+                    ({ id, movieTitle, previousActor, actorSelection, children }, index) => (
+                        <TreeNode
+                            key={id}
+                            movieTitle={movieTitle}
+                            previousActor={previousActor}
+                            actorSelection={actorSelection}
+                            level={index}
+                            // level={index > 0 ? setLevel(prevLevel => prevLevel + 1) : level}
+                        >
+                            {/* If this node has children, recursively render them as well, indenting them one level farther in */}
+                            {children && (
+                                <DataTree
+                                    treeData={children}
+                                // level={setLevel(prevLevel => prevLevel + 1)}
+                                />
+                            )}
+                        </TreeNode>
+                    )
+                )}
         </ul>
-    </li>
-);
-
-const DataTree = ({ treeData, level = 0 }) => (
-    <ul>
-        {treeData
-        // filter out any nodes that dont have a movieTitle property
-        .filter(({ movieTitle }) => movieTitle)
-        .map(
-            ({
-                id,
-                movieTitle,
-                previousActor,
-                actorSelection,
-                children
-            }) => (
-                <TreeNode
-                    key={id}
-                    movieTitle={movieTitle}
-                    previousActor={previousActor}
-                    actorSelection={actorSelection}
-                    level={level}
-                >
-                    {/* If this node has children, recursively render them as well, indenting them one level farther in */}
-                    {children && (
-                        <DataTree
-                            treeData={children}
-                            level={prevLevel => prevLevel + 1}
-                        />
-                    )}
-                </TreeNode>
-            )
-        )}
-    </ul>
-);
+    );
+}
 
 export default DataTree;
