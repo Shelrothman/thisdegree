67975e5002c3973a0ef3d62f730cb9106cf1b07b
Shelby Rothman <shelby.rothman@aderant.com>
2023-01-10 14:13:09 -0800

    removal ability yay

--- webapp/src/components/game/display/TreeBuildContainer.jsx
+++ webapp/src/components/game/display/TreeBuildContainer.jsx
@@ -1,65 +1,43 @@
-import { useState, useEffect, Fragment } from "react";
+import { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import uuid from 'react-uuid';
 
 import { useGameContext } from "../../../contexts";
-import ActorCard from "./ActorCard";
 import MovieCard from "./MovieCard";
 
 
 function TreeBuildContainer() {
     const {
         currentMovieTitle,
-        currentActorBridge,
         readyToBuild,
-        formTypeMovie,
-        gameChange,
         actorA,
         movieList
     } = useGameContext();
 
-    // const [bridgeCards, setBridgeCards] = useState([]);
-    const [actorName, setActorName] = useState(readyToBuild ? currentActorBridge : actorA);
-    // const [movieName, setMovieName] = useState(currentMovieTitle);
 
     const [localMovieList, setLocalMovieList] = useState(movieList);
 
 
-
-    
-    //? useMemo in here?
-    // useEffect(() => {
-    //     if (readyToBuild) {
-    //         setBridgeCards((prevBridgeCards) => {
-    //             return [...prevBridgeCards,
-    //             <MovieCard key={uuid()} movie={movieName} actorName={actorName} />,];
-    //         });
-    //     }
-    // }, [movieName]);
-    // // ? should these useEffects be refactored?
-
-    useEffect(() => {
-        if (readyToBuild) {
-            setActorName(currentActorBridge);
-        }
-        else setActorName(actorA);
-    }, [currentActorBridge]);
-
-    //* i thihnk i should be mapping the movieLost like o did way back before refactor... like map it and then set the bridgeCards to that so THAT we can filter it out when we need to
-
-
-    // }, [gameChange]);
-
     useEffect(() => {
         setLocalMovieList(movieList);
+        // this way a new card renders at top of each round (upon new movie input)
+        // when a movie is removed from the global movieList, this useEffect will trigger and set the localMovieList to the new movieList
     }, [currentMovieTitle]);
+    // only want it to trigger when a new movie title is set. this way we reduce the number of times it is triggered to just the top of a round
+
+
     return (
         <Container id="tree-build-container" fluid>
             <Row>
                 {localMovieList.map((movie) => {
                     return (
-                        <MovieCard key={uuid()} lastActor={!readyToBuild ? actorA : movie.previousActor.name} movie={movie.movieTitle} actorName={movie.actorSelection.name} />
+                        <MovieCard
+                            key={uuid()}
+                            lastActor={movie.previousActor.name}
+                            movie={movie.movieTitle}
+                            actorName={movie.actorSelection.name}
+                        />
                     )
                 })}
             </Row>
