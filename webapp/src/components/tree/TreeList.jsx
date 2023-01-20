38c87067c9f9acb039cbb5f63d5c1bb7586489fa
Shelby Rothman <shelby.rothman@aderant.com>
2023-01-20 10:54:33 -0800

    lifted fetch logic outside compon tList

--- webapp/src/components/tree/TreeList.jsx
+++ webapp/src/components/tree/TreeList.jsx
@@ -1,39 +1,39 @@
-// import { useQuery, gql } from '@apollo/client';
-// import { gql } from 'graphql-request';
-import { useQuery } from '@tanstack/react-query';
-import { GraphQLClient } from 'graphql-request';
 import Tree from './Tree';
 import Spinner from '../../utils/Spinner';
 
-const endpoint = 'http://localhost:4000';
-const graphQLClient = new GraphQLClient(endpoint, {
-    headers: {
-        authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI3YTZlZGJiMC1iMDg2LTQyMDgtOTYwYi0yOTBhYmJjZjkzMTQiLCJpYXQiOjE2NzE2NDE4MDN9.z3P8HRQM1oORsoFZn7BVFFtI8g6qWNXSecYLQQuypYc',
-    },
-})
+import { useTreeFeed} from '../../hooks/useQueryGQL';
 
-const TREE_QUERY = `
-{
-treeFeed {
-    trees {
-        id
-        treeDeclaration
-        postedBy {
-            id
-            name
-            email
-        }
-    }
-        count
-    }
-}`;
+// const endpoint = 'http://localhost:4000';
+// const graphQLClient = new GraphQLClient(endpoint, {
+//     headers: {
+//         authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI3YTZlZGJiMC1iMDg2LTQyMDgtOTYwYi0yOTBhYmJjZjkzMTQiLCJpYXQiOjE2NzE2NDE4MDN9.z3P8HRQM1oORsoFZn7BVFFtI8g6qWNXSecYLQQuypYc',
+//     },
+// })
+// const TREE_QUERY = gql`
+// {
+// treeFeed {
+//     trees {
+//         id
+//         treeDeclaration
+//         postedBy {
+//             id
+//             name
+//             email
+//         }
+//     }
+//         count
+//     }
+// }`;
 
 
 function TreeList() {
-    const { data, isLoading, error } = useQuery({
-        queryKey: ['treeFeed'],
-        queryFn: () => graphQLClient.request(TREE_QUERY),
-    });
+    // const { data, isLoading, error } = useQuery({
+    //     queryKey: ['treeFeed'],
+    //     queryFn: () => graphQLClient.request(TREE_QUERY),
+    // });
+
+    const { data, isLoading, error } = useTreeFeed();
+
     return (
         <div>
             {isLoading && <Spinner />}
--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -49,6 +49,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <React.StrictMode>
                 <App />
             </React.StrictMode>
+            <ReactQueryDevtools />
         </QueryClientProvider>
     </BrowserRouter>
 );
