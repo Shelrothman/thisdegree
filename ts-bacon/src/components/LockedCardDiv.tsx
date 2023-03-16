47b1cbaa3a885bca3fbd974b29cdbcc7ab31a82b
shelby.rothman <shelby.rothman@aderant.com>
2023-03-16 15:27:25 -0700

    display of exit btn set up

--- ts-bacon/src/components/LockedCardDiv.tsx
+++ ts-bacon/src/components/LockedCardDiv.tsx
@@ -1,9 +1,17 @@
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import { BsArrowDownCircle, BsThreeDotsVertical } from 'react-icons/bs';
 import { useGameContext } from '../contexts/GameContext';
 
 function LockedCardDiv({ actor, movie, round }: any) {
     const { globalFormState } = useGameContext() as any;
+
+    const inputProps = {
+        readOnly: true,
+        type: 'text',
+        // name: 'actor',
+    };
+
 
     return (
         <div>
@@ -11,14 +19,17 @@ function LockedCardDiv({ actor, movie, round }: any) {
                 backgroundColor: 'darkgreen',
                 border: '1px solid darkblue',
             }}>
-                <Card.Header>Round {round}:</Card.Header>
+                <Card.Header style={{ position: "relative" }}>
+                    Round {round}:
+                    <Button className="close-btn" aria-label="Close">
+                        &times;
+                    </Button>
+                </Card.Header>
                 <Card.Body>
-                    {/* <Card.Title>Info Saved</Card.Title> */}
                     <div>
                         <label htmlFor="actor">Actor:</label>{' '}
                         <input
                             type="text"
-                            // id="actor"
                             className="actor-input-locked"
                             name="actor"
                             value={actor}
@@ -29,23 +40,17 @@ function LockedCardDiv({ actor, movie, round }: any) {
                         <label htmlFor="movie">Movie:</label>{' '}
                         <input
                             type="text"
-                            // id="movie"
                             className="movie-input-locked"
                             name="movie"
                             value={movie}
                             readOnly
                         />
                     </div>
-                        <BsArrowDownCircle size='1.2rem' style={{
-                            // height: '1.5rem',
-                            paddingTop: '0.3rem',
-                        }} />
+                    <BsArrowDownCircle size='1.2rem' style={{ paddingTop: '0.3rem' }} />
                 </Card.Body>
             </Card>
             <BsThreeDotsVertical
-            // size='2rem'
                 style={{
-                    // height: '1.rem',
                     backgroundColor: 'darkgreen',
                     color: 'darkblue',
                     border: '1px solid darkblue',
