6e7b8ce640fe50eb49e5dbe22a9f4fd0cc581779
shelby.rothman <shelby.rothman@aderant.com>
2023-03-22 13:16:32 -0700

    focusing movieInput

--- ts-bacon/src/components/Close.tsx
+++ ts-bacon/src/components/Close.tsx
@@ -1,16 +1,13 @@
-import { ReactComponentElement, ReactElement } from 'react';
 import Button from 'react-bootstrap/Button';
 import { useGameContext } from '../contexts/GameContext';
 
-// TODO: the x button needs to coose ALL not just hthe one
-// currently its only removing the last one
+
 
 function Close({ round }: { round: number }) {
-    //* so we already have another component that will react to changes in thelockedCards array.. ie will lessen if array is lesesns and increasd.. so here we just need logic to remove the item from the array that was clicked on AND all the items below it. aka all the items with a higher index 
+    //* already have another component that will react to changes in thelockedCards array
     const { lockedCards, setLockedCards } = useGameContext() as any;
     function handleCloseClick(e: any) {
         const prevCards = [...lockedCards];
-        // console.log(prevCards);
         const target: any = e.target;
         let indexPlace: number;
         if (target) {
--- ts-bacon/src/components/UnlockedCard.tsx
+++ ts-bacon/src/components/UnlockedCard.tsx
@@ -1,23 +1,25 @@
-// import { useState } from 'react';
-// import Button from 'react-bootstrap/Button';
+import { useRef, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import { useGameContext } from '../contexts/GameContext';
 import Submit from './Submit';
 import { v4 as uuidv4 } from 'uuid';
 
-// TODO: maek the movie input autofucus when it renders
-
 // TODO obviously this is just a placeholder for now
 const testActors = ["Fake Cruise", "Tom Fanks", "Julia Fakerts", "Angelina Fakie", "Leonardo DiCaprico", "Sandra Bullocky", "Brad Pitiful", "Meryl Streepish", "Johnny Depressed", "Nicole Fakeman"];
 // TODO  make a 'select from currentMovie' title thang */}
 function UnlockedCardDiv({ actor, movie, round }: any = {}) {
     // start off empty since its unlocked
+    const inputRef = useRef<HTMLInputElement>(null);
     const {
         shakeInitiated,
         globalFormState,
         setGlobalFormState,
     } = useGameContext() as any;
+
+    useEffect(() => {
+        if (inputRef.current) inputRef.current.focus();
+    });
 
     const selectOptions = testActors.map((actor: string) => {
         return <option value={actor} key={uuidv4()}>{actor}</option>
@@ -52,7 +54,7 @@ function UnlockedCardDiv({ actor, movie, round }: any = {}) {
                 </div>
                 <div>
                     <label htmlFor="movie">Movie:</label>{' '}
-                    <Form.Control {...formControlProps('movie')} />
+                    <Form.Control {...formControlProps('movie')} ref={inputRef} />
                 </div>
                 <Submit />
             </Card.Body>
